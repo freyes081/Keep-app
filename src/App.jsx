cbee836dcd2a141e0eb886fe5b085cae398dc1eb
Franco <freyes081@gmail.com>
2023-06-12 10:06:30 -0400

    add json server

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import Note from "./components/Note"
-import notes from "./components/notes"
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import notes from "./components/notes";
+import { useState, useEffect } from "react";
 
 const App = () => {
-  
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8000/notes")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setNotes(data);
+      });
+  }, []);
+
   return (
     <>
-    <Header/>
-    <Footer/>
-    <Note/>
-    
+      <Header />
+      <Footer />
+      <Note notes={notes} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,16 +1,15 @@
-import notes from "./notes"
 
-const Note = () => {
+
+const Note = ({notes}) => {
   return (
     <div>
-     {notes.map((note) => {
-      return (
+     {notes.map(note => 
+      
         <div key={note.id} className="note">
           <h2>{note.title}</h2>
           <p>{note.content}</p>
         </div>
-      )
-     })} 
+           )} 
     </div>
   )
 }

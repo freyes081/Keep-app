ce8c20ce2402189c990b182c309da0824c013ed0
Franco <freyes081@gmail.com>
2023-06-13 08:21:18 -0400

    handle loading and error messages

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,34 @@ import { useState, useEffect } from "react";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   useEffect(() => {
     fetch("http://localhost:8000/notes")
       .then((res) => {
+        if (!res.ok) {
+          // error coming back from server
+          throw Error("could not fetch the data for that resource");
+        }
         return res.json();
       })
       .then((data) => {
         setNotes(data);
+        setLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        // auto catches network / connection error
+        setLoading(false);
+        setError(err.message);
       });
   }, []);
 
   return (
     <>
       <Header />
+      { error && <div>{ error }</div> }
+      {loading && <div className="loading"> Loading...</div>}
       <Footer />
       <Note notes={notes} />
     </>
